18e8cac41649e37818b3a0857553dc1a1cfa8c1b
iwrsu <tanishqkabir566@gmail.com>
2026-01-02 07:34:12 +0000

    potd 2nd jan

---
+++ potd_lc/961. N-Repeated Element in Size 2N Array_02_01_26.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto &i: nums) mp[i]++;
+        for(auto i: mp)
+        {
+            if(i.second==(nums.size()/2)) return i.first;
+        }
+        return -1;
+    }
+};

c82611b2ce4b4e66457eab2cf9d17b9fdd5261ef
iwrsu <tanishqkabir566@gmail.com>
2026-01-09 18:03:05 +0000

    daily lc and cf

--- cf_upsolve/B_Split.cpp
+++ cf_upsolve/B_Split.cpp
@@ -125,15 +125,27 @@ int binpow(int a, int b) {
 
 void solve()
 {
-    int a,b;
-    cin>>a>>b;
-    if((a&1)&&(b&1)) cout<<a*b+1;
-    else if(!(a&1)&&(b&1)) cout<<-1;
-    else {
-        int k=b/2;
-        int ans=(a*k)+(b/k);
-        cout<<((ans&1)?-1:ans);
+        int n;
+    cin >> n;
+    vector<int> a(2*n);
+    for(int i = 0;i < 2*n;i++){
+        cin >> a[i];
     }
+
+    vector<int> cnt(2*n+1);
+    for(auto &x : a)cnt[x]++;
+
+    int x = 0,y = 0,z = 0;
+    for(int i = 1;i <= 2*n;i++){
+        if(cnt[i] == 0)continue; 
+        if(cnt[i]&1)x++;         //odd elements
+        else if(cnt[i]%4)y++;    //element of form 4*k + 2
+        else z++;                //element of form 4*k 
+    }
+
+    int ans = x+2*y+2*z;
+    if((z&1) && x == 0)ans -= 2; 
+    cout << ans ;
 }
 
 int32_t main()
